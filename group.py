0a635738ec0533bcbb326497098390f64789b72f
chobo-1 <109354878+chobo-1@users.noreply.github.com>
2025-07-08 16:28:57 +0100

    Add files via upload

---
+++ group.py
@@ -0,0 +1,16 @@
+import random
+customer = []
+class BankAccount:
+    def __init__(self, name, acc_num, bal, ir):
+        self.name = name
+        self.account = acc_num
+        self.bal = bal
+        self.ir = ir
+
+def create_account(self, n: str, dep: int, ir: float) -> None:
+    customer.append(BankAccount(n, random.randint(10000000, 100000000), dep, ir))
+def deposit(self, acc: int, dep: int):
+    for ba in customer:
+        if ba.account == acc:
+            ba.bal +=dep
+            balance
